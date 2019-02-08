952bbbf9492c04809459fb4ce98c7908a1e0b312
SenseiFavour <fchukwuedo@gmail.com>
2019-02-08 02:28:07 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'toor',
-	'database' => 'shop',
+	'hostname' => '169.255.59.15',
+	'username' => 'chugonli_adomkrt',
+	'password' => 'oWMTK}4Ezd+R',
+	'database' => 'chugonli_admkther',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
